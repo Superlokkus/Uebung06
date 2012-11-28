146ff4d713d6aa20bcda0c7065495c7820a91bab
Markus Klemm <markus@markusklemm.net>
2012-11-28 17:41:50 +0100

    Task 1 completed

--- U06.py
+++ U06.py
@@ -4,3 +4,18 @@
 """Übung 06 Pendel"""
 #Markus Klemm WS12/13 Phy-BA
 
+import numpy
+import math
+
+#ToDo ArgParse
+
+FILENAME = "Pendel-Messung.dat"
+
+f = numpy.loadtxt(FILENAME)
+
+#Aufgabe 1
+print "Standardabweichung: " + str(numpy.std(f))
+print "Varianz: " + str(numpy.var(f))
+print "Standardabweichug des Mittelwerts: " + str(float(numpy.std(f)/math.sqrt(numpy.size(f))))
+
+
