02bb1eeba740229e8fa571ba6785a1225b2b18ea
Markus Klemm <markus@markusklemm.net>
2012-11-28 18:08:56 +0100

    Aufgabe 2 fertig

--- U06.py
+++ U06.py
@@ -14,8 +14,21 @@ FILENAME = "Pendel-Messung.dat"
 f = numpy.loadtxt(FILENAME)
 
 #Aufgabe 1
-print "Standardabweichung: " + str(numpy.std(f))
-print "Varianz: " + str(numpy.var(f))
-print "Standardabweichug des Mittelwerts: " + str(float(numpy.std(f)/math.sqrt(numpy.size(f))))
+print "Mittelwert: " + str(numpy.mean(f))
+print "Varianz: " + str(numpy.var(f)) + "s"
+print "Standardabweichug des Mittelwerts: " + str(float(numpy.std(f)/math.sqrt(len(f)))) + "s"
+
+#Aufgabe 2
+
+def myStat(myarray):
+    """Gibt den Mittelwert, die Varianz und die Standardabweichung zum Mittelwert, des übergebenen numpy.arrays, zurück"""
+    quer = str(numpy.mean(myarray))
+    var = str(numpy.var(myarray)) + "s"
+    sigmamittelwert = str(float(numpy.std(myarray)/math.sqrt(len(myarray)))) + "s"
+    return (quer,var,sigmamittelwert)
+   
+for x in range(len(f)+1):
+    tmp = f[:x+1]
+    myStat(tmp)
 
 
